b4dff5d5f7940ea769e7ea01e972471002b8862b
Melanie Ramos <Melanie Ramos@LAPTOP-6FN43BTQ>
2025-06-25 19:42:55 -0600

    Agregar archivos de proyecto.

---
+++ Examen_II/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Examen_II
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
